1df065654fc5e8200c9d97a42097acab2449bd79
gabrieleBatt <gabriele1.battaglia@mail.polimi.it>
2018-04-26 10:50:59 +0200

    pom update

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,6 +1,15 @@
 package it.polimi.ingsw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class AppTest
 {
+    @Test
+    public void testJunit(){
+        assertEquals(true, true);
+
+    }
 }
