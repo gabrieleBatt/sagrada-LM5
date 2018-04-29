b6f0e8db36405a10d696182a2f899ba788e2cf02
anna_argiro <annasofia.argiro@mail.polimi.it>
2018-04-29 17:04:13 +0200

    table finito

---
+++ src/main/java/it/polimi/ingsw/model/Table.java
@@ -0,0 +1,52 @@
+package it.polimi.ingsw.model;
+
+import javafx.scene.effect.Effect;
+
+import javax.tools.Tool;
+import java.util.List;
+
+public class Table {
+    private final List<Player> players;
+    private List<PublicObjective> publicObjectives;
+    private List<Tool> tools;
+    private final DiceBag diceBag;
+    private Pool pool;
+    private RoundTrack roundTrack;
+    private List<Effect> effects;
+
+    public Table(List<Player> players){
+        this.players = players;
+        diceBag = new DiceBag();
+    }
+    public void setPublicObjective(List<PublicObjective> publicObjective){
+        this.publicObjectives = publicObjective;
+    }
+    public void setTools(List<Tool> tools){
+        this.tools = tools;
+    }
+    public List<Player> getPlayers() {
+        return players;
+    }
+    public List<PublicObjective> getPublicObjectives() {
+        return publicObjectives;
+    }
+    public List<Tool> getTools() {
+        return tools;
+    }
+    public DiceBag getDiceBag(){
+        return diceBag;
+    }
+    public Pool getPool() {
+        return pool;
+    }
+    public RoundTrack getRoundTrack() {
+        return roundTrack;
+    }
+
+    public List<Effect> getActiveEffects() {
+        return effects;
+    }
+    public void addEffect(Effect effect){
+        effects.add(effect);
+    }
+}
