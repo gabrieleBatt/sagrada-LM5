c33619f6b56cebfb085100d9de12f84a379e4cd8
gabrieleBatt <gabriele1.battaglia@mail.polimi.it>
2018-05-09 11:09:17 +0200

    collapsed color and number set objective

--- src/main/java/it/polimi/ingsw/model/objective/SetPublicObjective.java
+++ src/main/java/it/polimi/ingsw/model/objective/SetPublicObjective.java
@@ -2,20 +2,23 @@ package it.polimi.ingsw.model.objective;
 
 import it.polimi.ingsw.model.exception.CellNotFoundException;
 import it.polimi.ingsw.model.exception.EmptyCellException;
+import it.polimi.ingsw.model.table.dice.DieColor;
 import it.polimi.ingsw.model.table.glassWindow.GlassWindow;
 
 import java.util.Collection;
 import java.util.HashSet;
 
-public class NumberSetPublicObjective extends PublicObjective {
+public class SetPublicObjective extends PublicObjective {
 
     private int points;
     private Collection<Integer> numbers;
+    private Collection<DieColor> colors;
 
-    public NumberSetPublicObjective(String name, int points, Collection<Integer> numbers) {
+    public SetPublicObjective(String name, int points, Collection<Integer> numbers, Collection<DieColor> colors) {
         super(name);
         this.points = points;
         this.numbers = new HashSet<>(numbers);
+        this.colors = new HashSet<>(colors);
     }
 
     @Override
@@ -28,6 +31,25 @@ public class NumberSetPublicObjective extends PublicObjective {
                     try {
                         if (glassWindow.getCell(i, j).isOccupied()){
                             if(glassWindow.getCell(i, j).getDie().getNumber() == n){
+                                newRet++;
+                            }
+                        }
+                    } catch (CellNotFoundException | EmptyCellException e) {
+                        System.out.println(e.getMessage());;
+                    }
+                }
+            }
+            if(ret > newRet){
+                ret = newRet;
+            }
+        }
+        for(DieColor dc: colors){
+            int newRet = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 5; j++) {
+                    try {
+                        if (glassWindow.getCell(i, j).isOccupied()){
+                            if(glassWindow.getCell(i, j).getDie().getColor().equals(dc)){
                                 newRet++;
                             }
                         }
