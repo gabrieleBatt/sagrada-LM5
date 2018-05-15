d120afeae9746261424d7abaeeb3c6e7c1957382
gabrieleBatt <gabriele1.battaglia@mail.polimi.it>
2018-05-15 14:49:08 +0200

    Removed Colors form DieColor

--- src/main/java/it/polimi/ingsw/server/model/table/dice/DieColor.java
+++ src/main/java/it/polimi/ingsw/server/model/table/dice/DieColor.java
@@ -1,33 +1,17 @@
 package it.polimi.ingsw.server.model.table.dice;
 
-import java.awt.*;
 
 /**
  *Die color is a concrete class containing an enum of the colors which a die can be characterized with.
  */
 public enum DieColor{
 
-    RED(Color.RED, "R"), GREEN(Color.GREEN, "G"), YELLOW(Color.YELLOW, "Y"), CYAN(Color.CYAN, "C"), MAGENTA(Color.MAGENTA, "M");
+    RED("R"), GREEN("G"), YELLOW("Y"), CYAN("C"), MAGENTA("M");
 
     private String toString;
-    private Color color;
 
-    /**
-     * Color assignment
-     * @param color
-     */
-    DieColor(Color color, String toString){
+    DieColor(String toString){
         this.toString = toString;
-        this.color = color;
-    }
-
-    /**
-     * Getting a die color
-     * @return: die color
-     */
-
-    public Color getColor(){
-        return color;
     }
 
     @Override
