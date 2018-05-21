c9ce76b2ef8c027ed400b4d6409fb4ec778ae708
gabrieleBatt <gabriele1.battaglia@mail.polimi.it>
2018-05-21 14:47:33 +0200

    Socket test from file

--- src/test/java/it/polimi/ingsw/server/controller/channels/SocketCommunicationChannelTest.java
+++ src/test/java/it/polimi/ingsw/server/controller/channels/SocketCommunicationChannelTest.java
@@ -7,8 +7,13 @@ import it.polimi.ingsw.server.model.table.Table;
 import org.junit.jupiter.api.*;
 
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class SocketCommunicationChannelTest{
@@ -17,24 +22,23 @@ class SocketCommunicationChannelTest{
     @DisplayName("Update test")
     @Test
     void update() throws IOException {
+        String fileOut = "resources/testResources/socketOut.txt";
+        String fileExpected = "resources/testResources/socketExpected.txt";
+
         Socket socket = new Socket();
-        BufferedReader reader = new BufferedReader(new FileReader("resources/testResources/socketOut.txt"));
-        PrintWriter o = new PrintWriter(new FileOutputStream("resources/testResources/socketOut.txt"));
+        PrintWriter o = new PrintWriter(new FileOutputStream(fileOut));
         SocketCommunicationChannel socketCommunicationChannel= new SocketCommunicationChannel(socket, null, o, "test");
-        Assertions.assertEquals("{\"result\":\"success\",\"header\":\"login\"}", reader.readLine());
         socketCommunicationChannel.updateView(new Pool());
-        Assertions.assertEquals("{\"pool\":[],\"header\":\"update\"}", reader.readLine());
         List<Player> players = new ArrayList<>();
         players.add(new Player("test"));
         players.add(new Player("test1"));
         socketCommunicationChannel.updateView(players.get(0), true);
-        Assertions.assertEquals("{\"header\":\"updatePlayer\",\"connection\":\"true\",\"player\":\"test\",\"token\":\"0\"}", reader.readLine());
         socketCommunicationChannel.updateView(players.get(1), true);
-        Assertions.assertEquals("{\"header\":\"updatePlayer\",\"connection\":\"true\",\"player\":\"test1\",\"token\":\"0\"}", reader.readLine());
         socketCommunicationChannel.updateView(new RoundTrack());
-        Assertions.assertEquals("{\"header\":\"update\",\"roundTrack\":[]}", reader.readLine());
         socketCommunicationChannel.updateView(new Table(players));
-        Assertions.assertEquals("{\"header\":\"update\",\"player\":[\"test\",\"test1\"]}", reader.readLine());
+        byte[] f1 = Files.readAllBytes(Paths.get(fileOut));
+        byte[] f2 = Files.readAllBytes(Paths.get(fileExpected));
+        Assertions.assertTrue(Arrays.equals(f1, f2));
     }
 
     @AfterEach
