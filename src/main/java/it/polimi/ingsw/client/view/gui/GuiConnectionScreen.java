3c70a529a542717fe1a8573731e36f75597bbaca
Bernardo.Casasole <bernardo.casasole@mail.polimi.it>
2018-06-28 19:05:58 +0200

    updated Gui connection screen - new layout

--- src/main/java/it/polimi/ingsw/client/view/gui/GuiConnectionScreen.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/GuiConnectionScreen.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.client.view.gui;
 
+import com.sun.prism.paint.Color;
 import it.polimi.ingsw.client.view.LoginInfo;
 import it.polimi.ingsw.client.view.factory.ConnectionScreen;
 import it.polimi.ingsw.client.Client;
@@ -7,11 +8,15 @@ import it.polimi.ingsw.shared.Message;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -19,12 +24,45 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import static javafx.scene.paint.Color.WHITE;
+import static javafx.scene.paint.Color.YELLOW;
 
 
 public class GuiConnectionScreen extends ConnectionScreen {
 
+    private static final int MIN_PORT = 1023;
+    private static final int MAX_PORT = 65536;
+    private static final int MAX_LENGHT = 12;
+    public static double WIDTH;
+    public static double HEIGHT;
+    private static final double DEFAULT_HEIGHT = 680;
+    private static final double DEFAULT_WIDTH  = 1280;
+    private static final double SMALL_SPACING;
+    private static final double BIG_SPACING;
+    private static final double VBOX_SPACING;
+    private static final double BUTTON_WIDTH_SIZE;
+    private static final double BUTTON_HEIGHT_SIZE;
+
+    static {
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        WIDTH = primaryScreenBounds.getWidth();
+        HEIGHT = primaryScreenBounds.getHeight();
+
+        if (DEFAULT_WIDTH / DEFAULT_HEIGHT < WIDTH / HEIGHT)
+            WIDTH = DEFAULT_WIDTH * HEIGHT / DEFAULT_HEIGHT;
+        else
+            HEIGHT = DEFAULT_HEIGHT * WIDTH / DEFAULT_WIDTH;
+        VBOX_SPACING = 25/DEFAULT_WIDTH*WIDTH;
+        BIG_SPACING = 50/DEFAULT_WIDTH*WIDTH;
+        SMALL_SPACING = 5/DEFAULT_WIDTH*WIDTH;
+        BUTTON_WIDTH_SIZE = 75/DEFAULT_WIDTH*WIDTH;
+        BUTTON_HEIGHT_SIZE = 35/DEFAULT_WIDTH*WIDTH;
+
+    }
+
     private LoginInfo loginInfo = null;
     private GridPane grid;
+    private VBox connectionScreenVBox;
     private String RMI = "RMI";
     private String SOCKET = "SOCKET";
 
@@ -34,70 +72,82 @@ public class GuiConnectionScreen extends ConnectionScreen {
 
     public void setScene(){
         Platform.runLater(() -> {
+            connectionScreenVBox = new VBox();
             Stage stage = Client.getStage();
+            stage.setHeight(HEIGHT);
+            stage.setWidth(WIDTH);
             stage.setTitle(Message.START_GAME.toString());
-
+            stage.setX(Screen.getPrimary().getBounds().getMinX());
+            stage.setY(Screen.getPrimary().getBounds().getMinY());
             Integer number = ThreadLocalRandom.current().nextInt(1, 10000);
-
 
             grid = new GridPane();
             grid.setAlignment(Pos.BOTTOM_CENTER);
-            grid.setHgap(50);
-            grid.setVgap(15);
-            grid.setPadding(new Insets(25, 25, 25, 25));
+            grid.setHgap(BIG_SPACING);
+            grid.setVgap(SMALL_SPACING);
             grid.getHgap();
             Button btn = new Button(Message.PLAY.toString());
-            HBox hbBtn = new HBox(10);
-            btn.setMinSize(75,35);
-            hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-            hbBtn.getChildren().add(btn);
-            grid.add(hbBtn, 1, 12);
+            btn.setMinSize(BUTTON_WIDTH_SIZE,BUTTON_HEIGHT_SIZE);
+
+
 
 
             ChoiceBox box = new ChoiceBox();
             box.getItems().addAll(RMI, SOCKET);
             grid.add(box,1,4);
             final Text actiontarget = new Text();
+            setAlgerian(actiontarget);
+            actiontarget.setFill(YELLOW);
 
-            grid.add(actiontarget, 0, 11);
+            connectionScreenVBox.getChildren().add(grid);
+            connectionScreenVBox.getChildren().add(actiontarget);
+            connectionScreenVBox.getChildren().add(btn);
+            //invisible button to speed up connection
+            Button button = new Button();
+            button.setOnAction(event -> {
+                loginInfo = new LoginInfo(SOCKET, "Player"+number,50004,"127.0.0.1","Password"+number);
+                btn.setDisable(true);
+            });
+            button.setOpacity(0);
+            button.setAlignment(Pos.CENTER);
+            button.setMinSize(SMALL_SPACING,SMALL_SPACING);
+            connectionScreenVBox.getChildren().add(button);
+            connectionScreenVBox.setAlignment(Pos.BOTTOM_CENTER);
+            connectionScreenVBox.setSpacing(VBOX_SPACING);
 
-            Scene scene = new Scene(grid, 475, 630);
+            Scene scene = new Scene(connectionScreenVBox,WIDTH, HEIGHT);
             stage.setScene(scene);
             scene.getStylesheets().add
                     (GuiConnectionScreen.class.getResource("/clientResources/gui/Login.css").toExternalForm());
-
             List<TextField> textFieldArray = new ArrayList<>();
-            Text scenetitle = new Text(Message.WELCOME.toString());
-            scenetitle.setId("welcome-text");
-            grid.add(scenetitle, 0, 0, 2, 1);
 
-            Label userName = new Label(Message.CHOOSE_NICKNAME.toString());
-            userName.setId("actiontarget");
+            Text userName = new Text(Message.CHOOSE_NICKNAME.toString());
+            setAlgerian(userName);
             grid.add(userName, 0, 1);
 
             TextField userTextField = new TextField();
             grid.add(userTextField, 1, 1);
             textFieldArray.add(userTextField);
 
-            Label pw = new Label(Message.INSERT_PASSWORD.toString());
-            pw.setId("actiontarget");
+            Text pw = new Text(Message.INSERT_PASSWORD.toString());
+            setAlgerian(pw);
             grid.add(pw, 0, 2);
 
-            Label ip = new Label(Message.IP_NUMBER.toString());
-            ip.setId("actiontarget");
+            Text ip = new Text(Message.IP_NUMBER.toString());
+            setAlgerian(ip);
+            setAlgerian(ip);
             grid.add(ip, 0, 6);
 
-            Label cnn = new Label(Message.CHOOSE_CONNECTION.toString());
-            cnn.setId("actiontarget");
+            Text cnn = new Text(Message.CHOOSE_CONNECTION.toString());
+            setAlgerian(cnn);
             grid.add(cnn, 0, 4);
 
             TextField ipTextField = new TextField();
-            ip.setId("actiontarget");
             grid.add(ipTextField, 1, 6);
             textFieldArray.add(ipTextField);
 
-            Label port = new Label(Message.PORT_NUMBER.toString());
-            port.setId("actiontarget");
+            Text port = new Text(Message.PORT_NUMBER.toString());
+            setAlgerian(port);
             grid.add(port, 0, 5);
 
             TextField portTextField = new TextField();
@@ -108,10 +158,9 @@ public class GuiConnectionScreen extends ConnectionScreen {
             PasswordField pwBox = new PasswordField();
             grid.add(pwBox, 1, 2);
 
-            btn.setOnAction(e -> {
-                actiontarget.setId("actiontarget");
 
-                loginInfo = new LoginInfo("SOCKET", "Player"+number,50004,"127.0.0.1","Password"+number);
+            btn.setOnAction(e -> {
+
 
                 if(box.getValue() == null || !isFilledOut(pwBox) || !isFilledOut(textFieldArray) || !isFilledOut(pwBox))
                     actiontarget.setText(Message.INCOMPLETE_FIELDS.toString());
@@ -190,7 +239,7 @@ public class GuiConnectionScreen extends ConnectionScreen {
      * @return boolean, true if nickname insert is valid.
      */
     private boolean validNickname(TextField userTextField){
-        return (!userTextField.getCharacters().toString().contains(" ")&& userTextField.getCharacters().length()<20);
+        return (!userTextField.getCharacters().toString().contains(" ")&& userTextField.getCharacters().length()<MAX_LENGHT);
     }
 
 
@@ -202,7 +251,7 @@ public class GuiConnectionScreen extends ConnectionScreen {
     private boolean validPort(TextField portTextField){
         String port = portTextField.getCharacters().toString();
         try {
-            return  (!port.contains(" ") && Integer.parseInt(port)>1023 && Integer.parseInt(port)<65536);
+            return  (!port.contains(" ") && Integer.parseInt(port)>MIN_PORT && Integer.parseInt(port)<MAX_PORT);
 
         } catch (NumberFormatException e) {
             return false;
@@ -215,7 +264,7 @@ public class GuiConnectionScreen extends ConnectionScreen {
      * @return boolean, true if password insert is valid.
      */
     private boolean validPassword(PasswordField pf){
-        return (!pf.getCharacters().toString().contains(" ") && pf.getCharacters().length()<20);
+        return (!pf.getCharacters().toString().contains(" ") && pf.getCharacters().length()<MAX_LENGHT);
     }
 
     /**
@@ -240,4 +289,8 @@ public class GuiConnectionScreen extends ConnectionScreen {
         return pf.getCharacters().length()>0;
     }
 
+    public void setAlgerian(Text algerian) {
+        algerian.setFill(WHITE);
+        algerian.setFont(Font.font("Algerian",20));
+    }
 }
